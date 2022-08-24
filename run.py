3bde5dcac7940bff3038d9826b417ce649f50067
Paula da Silva <carolpaulag@gmail.com>
2022-08-24 18:41:45 +0000

    Update worksheet

--- run.py
+++ run.py
@@ -70,7 +70,7 @@ def update_worksheet(data, worksheet):
     Receives a list of integers to be inserted into a worksheet
     Update the relevant worksheet with the data provided
     """
-    print(f"Updtating sales worksheet...\n")
+    print(f"Updating {worksheet} worksheet...\n")	
     worksheet_to_update = SHEET.worksheet(worksheet)
     worksheet_to_update.append_row(data)
     print(f"{worksheet} worksheet update successfully\n")
